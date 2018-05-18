4f1770a9691c7ecf4bf4fa98d785c2bbc37a4315
Dan Hammer <daniel.m.hammer@gmail.com>
2018-05-17 22:36:59 -0500

    Done

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,5 @@
 def is_armstrong(number):
-    pass
+    number_list = list(str(number))
+    power = len(number_list)
+    calculate = sum([int(x)**power for x in number_list])
+    return number == calculate
