a2f857b3531227772fd365351847719cd3244db8
DanMHammer <35697323+DanMHammer@users.noreply.github.com>
2018-05-18 09:49:49 -0500

    Done

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,26 @@
 class Allergies(object):
 
+
     def __init__(self, score):
-        pass
+        self._lst = []
+        allergy_dict = {1: "eggs", 2: "peanuts", 4: "shellfish",
+                        8: "strawberries", 16: "tomatoes", 32: "chocolate",
+                        64: "pollen", 128: "cats"}
+        key_list = [128, 64, 32, 16, 8, 4, 2, 1]
+
+        if score > 255:
+            score = score % 256
+
+        for item in key_list:
+            if score >= item:
+                self.lst.append(allergy_dict.get(item))
+                score -= item
+            else:
+                continue
 
     def is_allergic_to(self, item):
-        pass
+        return item in self.lst
 
     @property
     def lst(self):
-        pass
+        return self._lst
