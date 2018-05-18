846cba55debd832675e3067f60a11f351a4b6ded
DanMHammer <35697323+DanMHammer@users.noreply.github.com>
2018-05-18 15:51:35 -0500

    Submit 1

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,10 @@
+import re
+
 def abbreviate(words):
-    pass
+    words_list = re.split('\s|; |, |\*|\n|-', words)
+    acronym = ""
+
+    for word in words_list:
+        acronym += word[0].upper()
+
+    return acronym
