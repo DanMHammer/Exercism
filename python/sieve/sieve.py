850c5b134b4d9d66c52d5967ac794d457def8595
DanMHammer <35697323+DanMHammer@users.noreply.github.com>
2018-05-18 14:06:11 -0500

    1.97 seconds

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,2 +1,12 @@
 def sieve(limit):
-    pass
+
+    primes = [x for x in range(2, limit+1)]
+
+    for x in range(2, limit):
+        for y in range(2, limit):
+            try:
+                primes.remove(x*y)
+            except(ValueError):
+                continue
+
+    return primes
