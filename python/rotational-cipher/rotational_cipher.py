abfd14e265a14813e83e42bbfebe5b74e9fd2080
Dan Hammer <daniel.m.hammer@gmail.com>
2018-05-17 22:58:45 -0500

    Done

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,23 @@
+import re
+
+
 def rotate(text, key):
-    pass
+
+    # A-Z = 65 - 90 a-z = 97 - 122
+    encoded = ""
+
+    for char in text:
+        if re.match(r"[A-Z]", char):
+            if ord(char) + key > 90:
+                encoded += chr((ord(char)+key) % 90 + 64)
+            else:
+                encoded += chr(ord(char)+key)
+        elif re.match(r"[a-z]", char):
+            if ord(char) + key > 122:
+                encoded += chr((ord(char)+key) % 122 + 96)
+            else:
+                encoded += chr(ord(char)+key)
+        else:
+            encoded += char
+
+    return encoded
