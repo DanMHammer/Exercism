74533f9398b8b05e066bc73f728649259f5fbaf5
DanMHammer <35697323+DanMHammer@users.noreply.github.com>
2018-05-18 15:35:45 -0500

    Submit 1

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,8 @@
 def sum_of_multiples(limit, multiples):
-    pass
+
+    multiple_set = set()
+    
+    for number in multiples:
+        multiple_set = multiple_set.union([i for i in range(0, limit, number)])
+
+    return sum(multiple_set)
