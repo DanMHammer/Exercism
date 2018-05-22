3d92ce8d91fbebf0447372c208e5b5af2b29153e
Dan Hammer <daniel.m.hammer@gmail.com>
2018-05-21 21:57:53 -0500

    Incomplete

--- python/roman-numerals/roman_numerals.py
+++ python/roman-numerals/roman_numerals.py
@@ -1,2 +1,28 @@
 def numeral(number):
-    pass
+    output = ''
+
+    if number >= 1000:
+        output += 'M' * number//1000
+        number = number % 1000
+    if number >= 900:
+        output += 'CM'
+        number = number % 900
+    if number >= 500:
+        output += 'D' + ((number - 500) // 100) * 'C'
+        number = number % 500
+    if number >= 400:
+        output += 'CD'
+        number = number % 400
+    if number >= 100:
+        output += 'C' * number//100
+        number = number % 100
+    if number >= 90:
+        output += 'XC'
+        number = number % 90
+    if number >= 50:
+        output += 'L' + ((number - 50) // 10) * 'X'
+        number = number % 10
+    if number >= 40:
+        output += 'XL'
+        number = number % 40
+    if number 
