18771329c656ea815b80cf7dcdd6dccc4df08754
Dan Hammer <daniel.m.hammer@gmail.com>
2018-05-21 21:17:26 -0500

    Iteration 1

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,15 @@
-def flatten(iterable):
-    pass
+def flatten(iterable, limit=1000, counter=0):
+    for i in range(len(iterable)):
+        if (isinstance(iterable[i], (list, tuple)) and
+            counter < limit):
+            for a in iterable.pop(i):
+                iterable.insert(i, a)
+                i += 1
+            counter += 1
+            return flatten(iterable, limit, counter)
+    output = []
+    for elem in iterable:
+        if elem is not None:
+            output.append(elem)
+
+    return output
