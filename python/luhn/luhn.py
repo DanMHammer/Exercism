5fc6c9105230d3ae0734456cd51961e7fea301c2
dan.hammer <daniel.m.hammer@gmail.com>
2018-05-22 15:45:21 -0500

    Iteration 1

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,22 @@
 class Luhn(object):
     def __init__(self, card_num):
-        pass
-
+        try:
+            self.card_num = [int(x) for x in card_num.replace(" ", "")]
+        except(ValueError):
+            self.card_num = []
     def is_valid(self):
-        pass
+        if len(self.card_num) <= 1:
+            return False
+        sum_digits = 0
+        number = self.card_num[::-1]
+
+        for x in range(0, len(number)):
+            if x % 2 == 1:
+                if number[x] * 2 < 9:
+                    sum_digits += number[x] * 2
+                else:
+                    sum_digits += number[x] * 2 - 9
+            else:
+                sum_digits += number[x]
+
+        return sum_digits % 10 == 0
