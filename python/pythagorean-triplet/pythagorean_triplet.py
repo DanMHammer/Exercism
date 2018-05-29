36390e92652057a076336aa8136e53a5a36594fd
dan.hammer <daniel.m.hammer@gmail.com>
2018-05-29 14:35:39 -0500

    In Progress - 5/29/2018 2:35PM

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,10 +1,21 @@
+from math import gcd
+
 def primitive_triplets(number_in_triplet):
     pass
 
 
 def triplets_in_range(range_start, range_end):
-    pass
-
+    z = 0
+    triplets = set()
+    for c in range(range_start, range_end):
+        for a in range(range_start, range_end):
+            try:
+                b = round((c**2 - a**2)**.5)
+                if gcd(a, b) == 1:
+                    triplets.add(tuple(a, b, c))
+            except(TypeError):
+                pass
+    return triplets
 
 def is_triplet(triplet):
     pass
