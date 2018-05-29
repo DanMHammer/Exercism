76c1ab46142b95f3a8980f063e5e901442f5f3d5
dan.hammer <daniel.m.hammer@gmail.com>
2018-05-29 11:56:29 -0500

    Iteration 1

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,15 @@
+import re
+
 def score(word):
-    pass
+    total = 0
+    word = word.upper()
+
+    total += len(re.findall(r"[AEIOULNRST]", word))
+    total += len(re.findall(r"[DG]", word)) * 2
+    total += len(re.findall(r"[BCMP]", word)) * 3
+    total += len(re.findall(r"[FHVWY]", word)) * 4
+    total += len(re.findall(r"[K]", word)) * 5
+    total += len(re.findall(r"[JX]", word)) * 8
+    total += len(re.findall(r"[QZ]", word)) * 10
+
+    return total
