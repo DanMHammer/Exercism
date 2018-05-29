717b6242c32213188ef79a1af03c32c82ffc548d
dan.hammer <daniel.m.hammer@gmail.com>
2018-05-29 10:13:41 -0500

    Iteration 1

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,2 +1,26 @@
 def translate(text):
-    pass
+    words = text.lower().split()
+    pig = []
+    vowels  = ['a', 'e', 'i', 'o', 'u']
+
+    for word in words:
+        if word[0] in vowels or word[0:2] == 'xr' or word[0:2] == 'yt':
+            pig.append(word + 'ay')
+        elif word[0:2] == 'qu':
+            pig.append(word[2:] + word[0:2] + 'ay')
+        elif word[0] not in vowels and word[1:3] == 'qu':
+            pig.append(word[3:] + word[0:3] + 'ay')
+        elif word[0] not in vowels and word[1] == 'y':
+            pig.append(word[1:] + word[0] + 'ay')
+        elif word[0] not in vowels and word[1] not in vowels and word[2] == 'y':
+            pig.append(word[2:] + word[0:2] + 'ay')
+        else:
+            x = 0
+            for char in word:
+                if char not in vowels:
+                    x += 1
+                else:
+                    break
+            pig.append(word[x:] + word[:x] + 'ay')
+
+    return " ".join(pig)
