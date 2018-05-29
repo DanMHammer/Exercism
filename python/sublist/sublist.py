3eb969a54dcef2a83dd8094b28c5928cc23bcaf6
dan.hammer <daniel.m.hammer@gmail.com>
2018-05-29 13:41:07 -0500

    Iteration 1

--- python/sublist/sublist.py
+++ python/sublist/sublist.py
@@ -1,2 +1,20 @@
+#Does not pass test_unique_return_values
+
+SUBLIST = True
+SUPERLIST = True
+EQUAL = True
+UNEQUAL = True
+
 def check_lists(first_list, second_list):
-    pass
+
+    if first_list == second_list:
+        return EQUAL
+
+    if first_list in second_list:
+        return SUBLIST
+
+    if second_list in first_list:
+        return SUPERLIST
+
+    else:
+        return UNEQUAL
