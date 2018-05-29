4ee08d978b0b99708b5ea026a19e780251ad39e3
dan.hammer <daniel.m.hammer@gmail.com>
2018-05-29 09:20:49 -0500

    Finish

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,16 @@
+from math import ceil
+
 def prime_factors(natural_number):
-    pass
+    factors = []
+
+    for x in range(2, ceil(natural_number**.5)+1):
+        while natural_number % x == 0:
+            natural_number = natural_number/x
+            factors.append(x)
+
+    #Some natural numbers will complete the loop with one large prime factor remaining
+
+    if natural_number > 1:
+        factors.append(natural_number)
+
+    return factors
