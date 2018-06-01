3d42a55b7bc8222c18b047904c164fa501a17568
dan.hammer <daniel.m.hammer@gmail.com>
2018-06-01 11:39:16 -0500

    Start

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,25 @@
+import string
+import random
+
 class Robot(object):
     def __init__(self):
-        pass
+        self.database = set()
+        self.name = ""
+        self.new_name()
+
+    def new_name(self):
+        new = ""
+        for x in range(0, 2):
+            new += random.choice(string.ascii_uppercase)
+
+        for x in range(0, 3):
+            new += str(random.randint(0, 9))
+
+        if new not in self.database:
+            self.database.add(new)
+            self.name = new
+        else:
+            self.new_name()
+
+    def reset(self):
+        self.new_name()
